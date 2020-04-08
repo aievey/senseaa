25b33573fe4cc6dea4764e2bda923d73f71f4d6a
Amitabh Verma <amitabh283verma@gmail.com>
2020-04-08 20:57:22 +0530

    Updates

---
+++ precache-manifest.a4abf637614b94908887fe037db11c5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a8a6fae36ca747afa0edcfc1e5efe95",
+    "url": "/senseaa/index.html"
+  },
+  {
+    "revision": "9e5f87659f31843743ea",
+    "url": "/senseaa/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e05fca129028e05b510c",
+    "url": "/senseaa/static/js/2.f89cb76d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/senseaa/static/js/2.f89cb76d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e5f87659f31843743ea",
+    "url": "/senseaa/static/js/main.4b67f767.chunk.js"
+  },
+  {
+    "revision": "f348c48bdde8524b04c1",
+    "url": "/senseaa/static/js/runtime-main.bda4fcfd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/senseaa/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aievey.github.io/senseaa/precache-manifest.915ec1a6858d86e51ceed6e99ed40395.js"
+  "/senseaa/precache-manifest.a4abf637614b94908887fe037db11c5f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aievey.github.io/senseaa/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/senseaa/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
